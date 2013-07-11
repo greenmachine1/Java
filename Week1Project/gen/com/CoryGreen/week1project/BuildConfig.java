400a88398962858b631f4d2980371dfdf2b260aa
Cory <greenmachine@fullsail.edu>
2013-07-11 04:33:54 -0700

    added my week1 project apk

--- Week1Project/gen/com/CoryGreen/week1project/BuildConfig.java
+++ Week1Project/gen/com/CoryGreen/week1project/BuildConfig.java
@@ -1,12 +1,4 @@
-/*
- * Project		Week1Project
- * 
- * Package		com.CoryGreen.week1project
- * 
- * @author		Cory Green
- * 
- * date			Jul 11, 2013
- */
+/** Automatically generated file. DO NOT MODIFY */
 package com.CoryGreen.week1project;
 
 public final class BuildConfig {
