9bead8346e73e92e678c5bcb3086293eeb5f7016
Cory <greenmachine@fullsail.edu>
2013-07-11 04:10:09 -0700

    getting closer to my sorting of 3 numbers

--- Week1Project/src/com/CoryGreen/week1project/MainActivity.java
+++ Week1Project/src/com/CoryGreen/week1project/MainActivity.java
@@ -127,16 +127,49 @@ public class MainActivity extends Activity {
     // this is a method used to sort the three numbers passed in
     public String returnSortedNumbers(int first, int second, int third)
     {
-    	int number1;
-    	int number2;
-    	int number3;
+    	int number1 = 0;
+    	int number2 = 0;
+    	int number3 = 0;
     	
-    	for(int i = 0; i < 3; i++)
+    	for(int i = 0; i < 2; i++)
     	{
+    		if(first > second)
+    		{
+    			number1 = second;
+    			number2 = first;
+    			
+    			if(first > third)
+    			{
+    				number1 = third;
+    				number3 = first;
+    			}
+    			else if(first < third)
+    			{
+    				number1 = first;
+    				number3 = third;
+    			}
+    		}
+    		else if(first < second)
+    		{
+    			number1 = first;
+    			number2 = second;
+    			
+    			if(second > third)
+    			{
+    				number2 = third;
+    				number3 = second;
+    			}
+    			else if(second < third)
+    			{
+    				number2 = second;
+    				number3 = third;
+    			}
+    		}
     		
     	}
     	
-    	String tempReturnString = first + " " + second + " " + third;
+    	
+    	String tempReturnString = number1 + " " + number2 + " " + number3;
     	
     	return tempReturnString;
     }
