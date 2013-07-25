76edc7943daff806b2ec652904d075da36aaa7cb
Cory <greenmachine@fullsail.edu>
2013-07-25 01:24:09 -0700

    adding my searchForm button functionality

--- Week3Project/src/com/Cory/week3project/MainActivity.java
+++ Week3Project/src/com/Cory/week3project/MainActivity.java
@@ -3,7 +3,11 @@ package com.Cory.week3project;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
@@ -18,16 +22,31 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		// setting up my LinearLayout
 		_appLayout = new LinearLayout(this);
 		
+		// setting context to = this
 		_context = this;
 		
+		// calling on my SearchForm class
 		_searchForm = new SearchForm(_context, "Enter the name of a band", "Go");
 		
+		// adding _searchForm to my view
 		_appLayout.addView(_searchForm);
 		
+		// creating a button that gets the button within _searchForm
+		Button searchButton = _searchForm.getButton();
 		
-		
+		// creating my onClickListener for my searchButton
+		searchButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				Log.i("Button Pressed", "yes");
+				
+			}
+			
+		});
 		
 		
 		setContentView(_appLayout);
